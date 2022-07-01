f0b2f6c16e412bbb12e26bf070f72df2b9033dc4
Victor8825 <victorrv2588@gmail.com>
2022-07-01 23:38:03 +0300

    finish everything

--- src/js/buildMarkup.js
+++ src/js/buildMarkup.js
@@ -1,18 +1,18 @@
 export function buildMarkup(imagesArray) {
   return imagesArray.map( ({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
-  `<div class="photo-card">
+  `<div class="gallery__item">
   <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy"/></a>
-  <div class="info">
-    <p class="info-item">
+  <div class="gallery__info">
+    <p class="gallery__info-item">
       <b>Likes</b><br>${likes}
     </p>
-    <p class="info-item">
+    <p class="gallery__info-item">
       <b>Views</b><br>${views}
     </p>
-    <p class="info-item">
+    <p class="gallery__info-item">
       <b>Comments</b><br>${comments}
     </p>
-    <p class="info-item">
+    <p class="gallery__info-item">
       <b>Downloads</b><br>${downloads}
     </p>
   </div>
--- src/js/smoothScroll.js
+++ src/js/smoothScroll.js
@@ -3,7 +3,7 @@ const galleryRef = document.querySelector(".gallery");
 export function smoothScroll () {
   const { height: cardHeight } = 
     galleryRef.firstElementChild.getBoundingClientRect();
-    window.scrollBy(0,{
+    window.scrollBy({
     top: cardHeight * 2,
     behavior: "smooth",
   });
