d7ed21c77f61f5b248dd58baba6417e3765aa1a6
Victor8825 <victorrv2588@gmail.com>
2022-07-02 12:07:06 +0300

    bugs are fixed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import { Notify } from "notiflix/build/notiflix-notify-aio";
-import {resetPage} from "./js/resetPage";
-import {fetchImages} from "./js/fetchImages";
 import { addImagesToPage } from "./js/addImagesToPage";
-import { smoothScroll } from "./js/smoothScroll";
+import axios from "axios";
 
 
 const formRef = document.querySelector(".search-form");
 const scroolControlRef = document.querySelector(".scroll-control");
+const galleryRef = document.querySelector(".gallery");
+
 let inputText = "";
-let currentHits = 0;
+let totalHits = 0;
+const DEFAULT_PAGE = 1;
+const PER_PAGE = 40;
+let page = DEFAULT_PAGE;
+axios.defaults.baseURL = "https://pixabay.com/api/";
 
 
 formRef.addEventListener("submit", getRequestedImages);
@@ -19,30 +23,28 @@ function getRequestedImages (event) {
   if(inputText === "") {
     return Notify.warning("Please type the text in the search bar!");
   }
-  resetPage();
+
   fetchImages(inputText)
   .then(response => {
+      page = DEFAULT_PAGE;
+      galleryRef.innerHTML = "";
+
+      window.scrollTo(0,0);
+      totalHits = response.totalHits;
+    
       if(response.hits.length === 0 ) {
       return Notify.failure("Sorry, there are no images matching your search query. Please try again.");
       }
-      addImagesToPage(response);
-      currentHits += response.hits.length;
-      scroolControlRef.classList.remove("is-hidden");
 
-      smoothScroll();
-      Notify.info(`Hooray! We found ${response.totalHits} totalHits images`);
-      
-      ifThereAreImages (response);
-      
+      addImagesToPage(response);
+      scroolControlRef.classList.remove("is-hidden");
+      Notify.info(`Hooray! We found ${response.totalHits} totalHits images`);     
   })
   .catch(error => console.log(error));
 };
 
-function ifThereAreImages (response) {
-  if(currentHits === response.totalHits) {
-    setTimeout(() =>
-    Notify.info("We're sorry, but you've reached the end of search results."), 1000);
-  }
+function isThereNextPage () {
+  return page <= Math.ceil(totalHits / PER_PAGE);
 }
 
 const observerOptions = {
@@ -53,11 +55,31 @@ const observerOptions = {
 const observer = new IntersectionObserver( (entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      fetchImages(inputText)
-      .then(response => addImagesToPage(response));
+      page += 1;
+
+      if (isThereNextPage()) {
+        fetchImages(inputText, page)
+        .then(response => addImagesToPage(response));
+      } else {
+        Notify.warning("We're sorry, but you've reached the end of search results.");
+      }
     }
   });
 }, observerOptions);
 
 observer.observe(scroolControlRef);
 
+async function fetchImages (inputText, page=1) {
+  const searchParams = new URLSearchParams({
+    key: "28318270-6b0da933c6f7f5e767acec2c6",
+    q:inputText,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: "true",
+    page: page,
+    per_page: PER_PAGE,
+  });
+
+  const response = await axios.get(`?${searchParams}`);
+  return response.data;
+};
--- src/js/resetPage.js
+++
@@ -1,8 +0,0 @@
-const DEFAULT_PAGE = 1;
-let page = DEFAULT_PAGE;
-const galleryRef = document.querySelector(".gallery");
-
-export function resetPage() {
-  page = DEFAULT_PAGE;
-  galleryRef.innerHTML = "";
-};
